30880988530dd4f0a5cebb6beb204953f7b957d5
Brayan <brayan823@hotmail.com>
2018-08-29 00:25:37 -0300

    c

--- src/test/java/com/exercise/dashboard/test/service/TemperatureServiceTest.java
+++ src/test/java/com/exercise/dashboard/test/service/TemperatureServiceTest.java
@@ -43,7 +43,6 @@ public class TemperatureServiceTest {
 		when(env.getProperty("api.key")).thenReturn("f494fe4658955ce33e8331352513e2d2/");
 		when(env.getProperty("url.exclude")).thenReturn("?exclude=minutely,hourly,alerts,flags,currently");
 		when(template.getForObject(any(String.class), any(Class.class))).thenReturn(new ResponseEntity(new Weather(), HttpStatus.OK));
-        // I search for goku but system will use mocked response containing only ken, so I can check that mock is used.
         List<Temperature> weather = temperatureService.obtainTemperature("Caracas", "Celsius", 1);
         assertThat(weather).isNotNull();
 
